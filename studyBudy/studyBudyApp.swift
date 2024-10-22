b2b93a14dd4b816ead4b5afbbd5aed2e4649c8ab
fer <a00837146@tec.mx>
2024-10-22 09:30:31 -0600

    Initial commit

---
+++ studyBudy/studyBudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyBudyApp.swift
+//  studyBudy
+//
+//  Created by Geidy Vásquez on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct studyBudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
