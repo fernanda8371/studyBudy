99e398177bb67ac7cecee907e43eb46b4dac8e31
fer <a00837146@tec.mx>
2024-10-23 12:37:36 -0600

    log in

--- studyBudy/studyBudyApp.swift
+++ studyBudy/studyBudyApp.swift
@@ -5,10 +5,19 @@
 //  Created by Geidy Vásquez on 22/10/24.
 //
 
+import Foundation
 import SwiftUI
+import SwiftData
+import Firebase
+import GoogleSignIn
 
 @main
 struct studyBudyApp: App {
+    init() {
+            // Firebase initialization
+            FirebaseApp.configure()
+        }
+    
     var body: some Scene {
         WindowGroup {
             WelcomeView()
