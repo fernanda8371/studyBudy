5352fed7b26d4d41f87dd5b06cab29cb547e71a1
Maria Renee Ramos Valdez <133787165+reneeramosss@users.noreply.github.com>
2024-11-14 09:55:46 -0600

    mas cirfculos
    .

---
+++ studyBudy/Views/Graphs/PomodoroMetric.swift
@@ -0,0 +1,18 @@
+//
+//  PomodoroMetric.swift
+//  studyBudy
+//
+//  Created by Maria Renee Ramos Valdez on 14/11/24.
+//
+
+import SwiftUI
+
+struct PomodoroMetric: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PomodoroMetric()
+}
